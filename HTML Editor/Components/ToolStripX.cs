3e92c6f6178e43b7449f36b88cc381e74498fcb6
beerkaya <beerkaya@github.com>
2021-05-19 13:09:04 +0300

    Add project files.

---
+++ HTML Editor/Components/ToolStripX.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTML_Editor.Components
+{
+    public partial class ToolStripX : UserControl
+    {
+        //public OpenFileDialog openFile { get; set; }
+        //public SaveFileDialog saveFile { get; set; }
+        private RichTextBoxX richTextBoxX;
+        public ToolStripX(RichTextBoxX richTextBoxX)
+        {
+            this.richTextBoxX = richTextBoxX;
+            InitializeComponent();
+        }
+    }
+}
